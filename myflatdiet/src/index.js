944dd44016acb0d8d326a0561b9a38f875df1ac3
Brian <Bemi713@gmail.com>
2022-04-19 09:46:20 -0500

    Created REACT FRONTEND AND SET UP CARDS

---
+++ myflatdiet/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Components/index.css';
+import App from './Components/App.js';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
