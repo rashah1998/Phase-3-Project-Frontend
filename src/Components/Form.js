983ff32a5611a7f3c0f9f772b2d6deeec6cad917
Rahul Shah <rashah1998@yahoo.com>
2022-04-22 12:22:03 -0400

    Fixed issue with POST to FoodItem.diets

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -25,9 +25,6 @@ function Form({foodItems, setFoodItems}) {
   function handleSubmit(e) {
     e.preventDefault()
     
-    const dietArr = []   
-    document.querySelectorAll("input[type=checkbox]").forEach(diet => diet.checked ? dietArr.push(diet.name) : null)
-    setNewFoodItem({...newFoodItem, diets: [...dietArr]})
     fetch("http://localhost:9292/fooditems", {
       method: "POST",
       headers: {
@@ -37,14 +34,13 @@ function Form({foodItems, setFoodItems}) {
       body: JSON.stringify(newFoodItem)
     }).then(resp => resp.json())
     .then(newItem => setFoodItems([...foodItems, newItem]))
-
+    console.log(newFoodItem)
     e.target.reset();
   }
 
   return (
     <div id='form-background'>
       <form onSubmit={handleSubmit}>
-        {console.log(newFoodItem)}
         <h5><label htmlFor="name of item">Name of Item:</label></h5>
         <input type="text" name="item_name" id="new-item-name" onChange={handleChange} required/>
         <h5><label htmlFor="image">Image URL:</label></h5>
@@ -55,21 +51,21 @@ function Form({foodItems, setFoodItems}) {
         <input type="number" min="1" name="number_of_calories" onChange={handleChange} required/>
 
         <h4>Choose the Diets for this Food:</h4>
-        <input className='form-checkbox' type="checkbox" id="Carnivorous" name="Carnivorous" value="Carnivorous"/>
+        <input className='form-checkbox' type="checkbox" id="Carnivorous" name="Carnivorous" value="Carnivorous" onChange={handleChange}/>
         <h6><label htmlFor="Carnivorous"> Carnivorous </label></h6><br/>
-        <input className='form-checkbox' type="checkbox" id="Keto" name="Keto" value="Keto"/>
+        <input className='form-checkbox' type="checkbox" id="Keto" name="Keto" value="Keto" onChange={handleChange}/>
         <h6><label htmlFor="Keto"> Keto </label></h6><br/>
-        <input className='form-checkbox' type="checkbox" id="Pescatarian" name="Pescatarian" value="Pescatarian"/>
+        <input className='form-checkbox' type="checkbox" id="Pescatarian" name="Pescatarian" value="Pescatarian" onChange={handleChange}/>
         <h6><label htmlFor="Pescatarian"> Pescatarian </label></h6><br/>
-        <input className='form-checkbox' type="checkbox" id="Vegan" name="Vegan" value="Vegan"/>
+        <input className='form-checkbox' type="checkbox" id="Vegan" name="Vegan" value="Vegan" onChange={handleChange}/>
         <h6><label htmlFor="Vegan"> Vegan </label></h6><br/>
-        <input className='form-checkbox' type="checkbox" id="Vegetarian" name="Vegetarian" value="Vegetarian"/>
+        <input className='form-checkbox' type="checkbox" id="Vegetarian" name="Vegetarian" value="Vegetarian" onChange={handleChange}/>
         <h6><label htmlFor="Vegetarian"> Vegetarian </label></h6><br/>
-        <input className='form-checkbox' type="checkbox" id="Mediterranean" name="Mediterranean" value="Mediterranean"/>
+        <input className='form-checkbox' type="checkbox" id="Mediterranean" name="Mediterranean" value="Mediterranean" onChange={handleChange}/>
         <h6><label htmlFor="Mediterranean"> Mediterranean</label><br/></h6>
-        <input className='form-checkbox' type="checkbox" id="High Protein" name="High Protein" value="High Protein"/>
+        <input className='form-checkbox' type="checkbox" id="High Protein" name="High Protein" value="High Protein" onChange={handleChange}/>
         <h6><label htmlFor="High Protein"> High Protein</label></h6><br/>
-        <input className='form-checkbox' type="checkbox" id="Paleo" name="Paleo" value="Paleo"/>
+        <input className='form-checkbox' type="checkbox" id="Paleo" name="Paleo" value="Paleo" onChange={handleChange}/>
         <h6><label htmlFor="Paleo"> Paleo </label></h6><br/>
         <input className='form-checkbox' type="submit" value="Submit" />
         <div id='circle-1'></div>
