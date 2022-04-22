fe1347ba859dc421739931f2f767f43e3d67cf63
Brian <Bemi713@gmail.com>
2022-04-22 11:06:48 -0500

    About Page Section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,42 @@
 
 
 function About() {
+
+<h2>
+   <div> 
+    Carnivorous 
+The carnivore diet is straightforward: eat only animal foods and stay away from all plant foods. This means that you get your energy primarily from protein and fat and consume close to zero carbohydrates. The carnivore diet means eliminating plant foods, so no fruits, vegetables, grains, nuts, seeds, or legumes.
+
+    Keto
+People on a ketogenic diet eat a very small amount of carbohydrates, a moderate amount of protein, and a high amount of fat per day. This means that the body burns fat as its main source of fuel and breaks it down into ‘ketone bodies’ (or ‘ ketones ’) in a process called ketosis.
+
+    Pescatarian 
+Pescatarians have a lot in common with vegetarians. They eat fruits, veggies, nuts, seeds, whole grains, beans, eggs, and dairy, and stay away from meat and poultry. But there's one way they part company from vegetarians: Pescatarians eat fish and other seafood.
+Vegetarian 
+A vegetarian diet should include a diverse mix of fruits, vegetables, grains, healthy fats, and proteins. To replace the protein provided by meat in the diet, one should include a variety of protein-rich plant foods like nuts, seeds, legumes, tempeh, tofu, and seitan.
+
+
+    Vegan 
+Veganism is a type of vegetarian diet that excludes meat, eggs, dairy products, and all other animal-derived ingredients. Many vegans also do not eat foods that are processed using animal products, such as refined white sugar and some wines. 
+
+    Mediterranean
+A Mediterranean-style diet typically includes:
+plenty of fruits, vegetables, bread and other grains, potatoes, 
+beans, nuts, and seeds olive oil as a primary fat source. 
+and dairy products, eggs, fish, and poultry in low to moderate amounts.
+
+Fish and poultry are more common than red meat in this diet. It also centers on minimally processed, plant-based foods. Wine may be consumed in low to moderate amounts, usually with meals. Fruit is a common dessert instead of sweets.
+
+High Protein
+High-protein diets typically include large quantities of protein and only a small amount of carbohydrates. Most people can follow a high-protein diet by eating meat, fish, dairy products, beans and legumes, eggs, and vegetables that are relatively rich in protein, such as asparagus and spinach.
+
+
+Paleo 
+A paleo diet typically includes lean meats, fish, fruits, vegetables, nuts, and seeds — foods that in the past could be obtained by hunting and gathering. A paleo diet limits foods that became common when farming emerged about 10,000 years ago. These foods include dairy products, legumes, and grains.
+    </div>
+</h2>
+
+
     return null;
 }
 
